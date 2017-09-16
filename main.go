968bff271cee3b47329c65ac094c905d611bc032
Trystan Johnson <trystan.johnson@gotinder.com>
2017-09-16 11:24:53 -0700

    print result

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import "github.com/trystanj/bazel-proto/helper"
 func main() {
 	log.Println("OH HAI")
 
-	helper.Help()
-
-	// log.PrintF("demo.very, %v", d.very)
+	help := helper.Help()
+	log.Printf("Helper: %v", help)
 }
