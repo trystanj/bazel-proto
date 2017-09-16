1cce0842a1c4b537aa6b19c76bde9a01f5488d62
Trystan Johnson <trystan.johnson@gotinder.com>
2017-09-16 10:18:13 -0700

    wtf

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import "log"
-import "github.com/trystanj/bazel-proto/proto/demo"
+import "github.com/trystanj/bazel-proto/helper"
 
 func main() {
 	log.Info("OH HAI")
 
+	helper.Help()
 	d := &demo.Demo{very: "hi", useful: 1}
 	log.Infof("demo.very, %v", d.very)
 }
