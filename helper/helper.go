ea2105d8955ed67126f5b3f9936f46f440878308
Trystan Johnson <trystan.johnson@gotinder.com>
2017-09-16 10:37:14 -0700

    wip

--- helper/helper.go
+++ helper/helper.go
@@ -1,10 +1,14 @@
 package helper
 
-import "log"
-import "github.com/trystanj/bazel-proto/proto/demo"
+import (
+	"log"
+
+	"github.com/trystanj/bazel-proto/proto/demo"
+)
 
 func Help() string {
 	d := &demo.Demo{very: "hi", useful: 1}
 	log.Infof("demo.very, %v", d.very)
+
 	return d.very
 }
